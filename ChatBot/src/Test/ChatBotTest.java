4f645fe1ddb079b01ac6fc9e470c432bb3298a1e
feria-de-armas <60250054+feria-de-armas@users.noreply.github.com>
2022-02-16 13:24:51 -0800

    Simple console chat bot
    Not general, simple question logic to create a person object and call to string and display the information gathered

---
+++ ChatBot/src/Test/ChatBotTest.java
@@ -0,0 +1,38 @@
+package Test;
+
+import com.company.ChatBot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatBotTest {
+
+    public ChatBotTest() {
+        ChatBot cb = new ChatBot();
+
+    }
+
+    @Test
+    void name() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void askByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGreeting() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getQ1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getQ2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getQ3() {
+    }
+}
---
+++ ChatBot/src/com/company/Person.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class Person {
+    private String name;
+    private int age;
+    private String occupation;
+    private String hobbies;
+    private String query;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", occupation='" + occupation + '\'' +
+                ", hobbies='" + hobbies + '\'' +
+                ", query(s)='" + query + '\'' +
+                '}';
+    }
+
+    public Person(String name, int age, String occupation, String hobbies, String problem) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.hobbies = hobbies;
+        this.query = problem;
+    }
+
+    public Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
