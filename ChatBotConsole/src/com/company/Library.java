5f44127f51f815c0fa6e42f3eabe3cba63fdc584
buttertoastt <richardombrown@outlook.com>
2022-02-22 16:53:01 -0800

    inital

--- ChatBotConsole/src/com/company/Library.java
+++ ChatBotConsole/src/com/company/Library.java
@@ -16,19 +16,16 @@ public class Library {
             d = new Book("Game of Thrones", "Fantasy", "300", "G.R.R. Martin");
             e = new Book("Odyssey", "classic", "300", "Homer");
             f = new Book("The Great Gatsby", "classic", "300", "F. Scott Fitzgerald");
-        g = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        h = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        i = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        j = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        k = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        l = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        m = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        n = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        o= new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-        p= new Book("Dune", "Sci-Fi", "300", "F. Hebert");
-
-
-
+            g = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            h = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            i = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            j = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            k = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            l = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            m = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            n = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            o = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
+            p = new Book("Dune", "Sci-Fi", "300", "F. Hebert");
 
     }
 
