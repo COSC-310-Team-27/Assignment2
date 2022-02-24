d78e6cd4049178e2f49f9286f329071d6d766554
feria-de-armas <60250054+feria-de-armas@users.noreply.github.com>
2022-02-23 16:31:43 -0800

    some dialog added to main

--- ChatBotConsole/src/com/company/PCA.java
+++ ChatBotConsole/src/com/company/PCA.java
@@ -1,9 +1,27 @@
 package com.company;
 
 public class PCA {
+    int [] userVector = new int[2];
     /*
         TODO
         implement PCA developed in jupyter nb
         return
         */
+
+    public int[] getUserVector() {
+        return userVector;
+    }
+
+    public void setUserVector(int[] userVector) {
+        this.userVector = userVector;
+    }
+
+    public String book() {
+        return "Dune";
+    }
+    public String genera() {
+        return "Action";
+    }
+
+
 }
