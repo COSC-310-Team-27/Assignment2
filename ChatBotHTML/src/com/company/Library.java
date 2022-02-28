b6abae4fe2fdb7f05d12eeb2669f101cc17a8b10
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-02-27 19:34:33 -0800

    Added books to library

--- ChatBotHTML/src/com/company/Library.java
+++ ChatBotHTML/src/com/company/Library.java
@@ -12,7 +12,7 @@ public class Library {
         //private ArrayList<Book> bookList = new ArrayList<>(Arrays.asList(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p));
         private ArrayList<Book> bookList = new ArrayList<>();
         public Library() {
-            bookList.add(new Book("Dune", "Sci-Fi", "300", "F. Hebert")); 
+            bookList.add(new Book("Dune", "Sci-Fi", "300", "F. Hebert"));
             bookList.add(new Book("Foundation", "Sci-Fi", "300", "I. Asimov"));
             bookList.add(new Book("Lord of the Rings", "Fantasy", "300", "J.R. Tolkien"));
             bookList.add(new Book("Game of Thrones", "Fantasy", "300", "G.R.R. Martin"));
@@ -34,6 +34,13 @@ public class Library {
             bookList.add(new Book("Overlord, Vol 4 - The Lizardman Heroes", "Light novel", "304", "Kugane Maruyama"));
             bookList.add(new Book("Overlord, Vol 5 - The Men Of The Kingdome Part 1", "Light novel", "272", "Kugane Maruyama"));
             bookList.add(new Book("Overlord, Vol 6 - The Men Of The Kingdome Part 2", "Light novel", "292", "Kugane Maruyama"));
+            bookList.add(new Book("Overlord, Vol 7 - The Invaders Of The Great Tomb", "Light novel", "350", "Kugane Maruyama"));
+            bookList.add(new Book("Overlord, Vol 8 - The Two Leaders", "Light novel", "288", "Kugane Maruyama"));
+            bookList.add(new Book("The return of Johnny Ridden MSV-R, Vol 1", "Manga", "350", "Kugane Maruyama"));
+            bookList.add(new Book("Gundam Thunderbolt, Vol 1", "Manga", "350", "Hajime Yadate"));
+            bookList.add(new Book("Gundam Thunderbolt, Vol 2", "Manga", "350", "Hajime Yadate"));
+            bookList.add(new Book("Gundam Thunderbolt, Vol 3", "Manga", "350", "Hajime Yadate"));
+
     }
 
     public ArrayList<Book> getBookList() {
